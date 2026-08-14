fc72e8b2308de42a6242bf18db62eb49d60b0655
Felix Fernando <felixzhuang1@gmail.com>
2026-08-14 11:49:13 +0700

    [FEATURE] Reduction Operations (#8)

--- src/root.zig
+++ src/root.zig
@@ -3,6 +3,7 @@ const std = @import("std");
 pub const NDArray = @import("core/ndarray.zig").NDArray;
 pub const creation = @import("core/creation.zig");
 pub const elementwise = @import("core/elementwise.zig");
+pub const reduce = @import("core/reduce.zig");
 pub const shape = @import("core/shape.zig");
 
 test {
